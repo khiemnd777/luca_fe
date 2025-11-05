96662884c053ba56f11c8c9edda64c5b21c18712
khiemnd777 <khiemnd777@gmail.com>
2025-11-05 19:29:52 +0700

    Logout button

--- src/features/auth/presentation/pages/account.page.tsx
+++ src/features/auth/presentation/pages/account.page.tsx
@@ -8,15 +8,26 @@ import type { AutoFormRef } from "@root/core/form/form.types";
 import AccountForm from "@features/auth/components/account-form";
 import AccountChangePasswordForm from "@features/auth/components/account-change-password-form";
 import { AutoGrid } from "@shared/components/ui/auto-grid";
+import { useAuthStore } from "@store/auth-store";
+import { LogoutRounded } from "@mui/icons-material";
 
 export default function AccountPage() {
+  const logout = useAuthStore((s) => s.logout);
   const formAccountRef = React.useRef<AutoFormRef>(null);
   const formAccountChangePasswordRef = React.useRef<AutoFormRef>(null);
   return (
     <>
       <BasePage>
         <PageContainer>
-          <PageToolbar title="Tài khoản" subtitle="Chỉnh sửa thông tin tài khoản đăng nhập." />
+          <PageToolbar title="Tài khoản" subtitle="Chỉnh sửa thông tin tài khoản đăng nhập."
+            actions={
+              <>
+                <Button variant="contained" color="error" startIcon={<LogoutRounded />} onClick={async () => await logout()}>
+                  Đăng xuất
+                </Button>
+              </>
+            }
+          />
           <AutoGrid>
             <SectionCard title={"Thông tin tài khoản"} extra={
               <Button variant="contained" onClick={() => formAccountRef.current?.submit()}>

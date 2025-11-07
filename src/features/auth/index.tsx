be5a1d8fbc5c46e37c48f032902793f01dd5d287
khiemnd777 <khiemnd777@gmail.com>
2025-11-07 22:24:40 +0700

    remove redundant code

--- src/features/auth/index.tsx
+++ src/features/auth/index.tsx
@@ -1,8 +1,6 @@
 import type { ModuleDescriptor } from "@root/core/module/types";
 import { registerModule } from "@root/core/module/registry";
 
-// const AccountPage = React.lazy(() => import("@features/auth/presentation/pages/account.page"));
-
 const mod: ModuleDescriptor = {
   id: "auth",
   routes: [
